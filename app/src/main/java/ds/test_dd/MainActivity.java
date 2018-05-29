1e9110fd56a27a114d7e79c3fa6a8153abfc18f9
sordono <santiordono@hotmail.com>
2018-05-29 23:29:11 +0200

    Añado otra variable
    y comentario

--- app/src/main/java/ds/test_dd/MainActivity.java
+++ app/src/main/java/ds/test_dd/MainActivity.java
@@ -7,10 +7,16 @@ public class MainActivity extends AppCompatActivity {
 
     private int pelusa;
     private int chanclas;
+    private int sonido;
+
+    //If sonido1=chanclas then "no estás chingando bien"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
