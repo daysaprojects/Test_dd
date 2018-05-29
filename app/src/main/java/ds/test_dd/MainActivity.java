0b88f5e71ad6e2a208de368a0c14a8facb2f5f4c
sordono <santiordono@hotmail.com>
2018-05-29 23:49:42 +0200

    Añado COMENTARIO Y TU TOAST

--- app/src/main/java/ds/test_dd/MainActivity.java
+++ app/src/main/java/ds/test_dd/MainActivity.java
@@ -2,6 +2,7 @@ package ds.test_dd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
