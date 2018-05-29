30526dd9bdeaa139362578272c6b005be9253b7a
sordono <santiordono@hotmail.com>
2018-05-29 23:22:01 +0200

    Añado otra variable

--- app/src/main/java/ds/test_dd/MainActivity.java
+++ app/src/main/java/ds/test_dd/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private int pelusa;
+    private int chanclas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
